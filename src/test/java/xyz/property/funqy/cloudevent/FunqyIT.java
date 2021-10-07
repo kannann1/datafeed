90a96295afa4439f863182b6cd7cde39be8e18fd
quarkusio <no-reply@quarkus.io>
2021-10-07 17:20:23 +0000

    Initial commit

---
+++ src/test/java/xyz/property/funqy/cloudevent/FunqyIT.java
@@ -0,0 +1,10 @@
+package xyz.property.funqy.cloudevent;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class FunqyIT extends FunqyTest {
+
+    // Run the same tests
+
+}
